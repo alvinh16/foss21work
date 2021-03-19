72d9886673aa1666f4d0d571b19a77a9367bf6f1
Alvin Ho <alvinho@yandex.com>
2021-03-19 19:10:45 +0800

    my first commit

---
+++ inexample.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.9
+
+i=0
+for j in range (1,9) :
+    i += 1
+    print ("value of i is now ", i)
---
+++ inputexample.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3.9
+
+age = input ("how old are you? ")
+print ("you are ", str(age), " years old")
